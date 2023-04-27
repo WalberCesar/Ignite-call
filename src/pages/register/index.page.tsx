a14d8e14e51ebaef83e5b0db66702f09f61122c9
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-04-27 17:48:29 -0300

    create user using method post

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/src/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -38,7 +39,16 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  async function handleRegister(data: RegisterFormSchema) {}
+  async function handleRegister(data: RegisterFormSchema) {
+    try {
+      await api.post('/users', {
+        username: data.username,
+        name: data.name,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <Container>
