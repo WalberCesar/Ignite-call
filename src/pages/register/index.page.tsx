400ba4a8af6505171e2a78d5c7c51a3adff01ddf
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-04-27 16:42:34 -0300

    create router.push and use query

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -4,6 +4,8 @@ import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const registerFormSchema = z.object({
   username: z
@@ -25,8 +27,16 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormSchema>({ resolver: zodResolver(registerFormSchema) })
+
+  const router = useRouter()
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormSchema) {}
 
